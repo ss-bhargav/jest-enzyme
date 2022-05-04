55d3782d962ccc9634c9df8c0db44f8a46b0ea8f
ss-bhargav <sankoji.saibhargav@gmail.com>
2022-05-04 18:16:05 +0530

    Demo

---
+++ demo/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders learn react link', () => {
+
+});
